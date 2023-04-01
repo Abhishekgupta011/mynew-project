d445b4db2f5c67f979ce23fb9dac485a59cd4ab4
Abhishekgupta011 <ag25061999@gmail.com>
2023-04-01 10:25:00 +0530

    delete button added

--- src/newcompo/Expenses/ExpenseItems.js
+++ src/newcompo/Expenses/ExpenseItems.js
@@ -3,7 +3,18 @@ import ExpenseDate from './ExpenseDate';
 import './Expenseitems.css';
 import Card from '../../UI/Card.js'
 const ExpenseItems = (props) =>{
-
+    const clickHandler = ()=>{
+        console.log('clicked')
+    }
+    const deleteExpenseHandler = () => {
+        const expenseItem = document.querySelector(`.expense`);
+        if (expenseItem) {
+            expenseItem.parentNode.removeChild(expenseItem);
+            console.log('delete');
+        } else {
+            console.log(`Element with ID expense not found.`);
+        }
+    }
     return (
         <Card className="expense">
             <ExpenseDate date = {props.date}/>
@@ -12,6 +23,8 @@ const ExpenseItems = (props) =>{
             location={props.location}   
             title={props.title}
              />
+             <button onClick={clickHandler}>Change Title</button>
+             <button onClick={deleteExpenseHandler}>Delete Expense</button>
         </Card>
     );
 };
